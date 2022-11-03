c35f5dfcf326fa110277a99a33fb6599fc5a6e63
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-03 14:10:11 +0200

    finished fist taks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+let intevalId = null;
+
+function onStart() {
+    refs.start.disabled = true;
+   intevalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()    
+    },1000)
+    
+
+}
+
+function onStop() {
+    if (refs.start.disabled) {
+        refs.start.disabled = false;
+        clearInterval(intevalId);
+    }
+    
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
