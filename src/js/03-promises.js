c4a19d66f3ad19f9a34b3eba9d82608af80fba0e
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-05 16:04:14 +0200

    Tru to do third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  button: document.querySelector('button')
 }
+const promiseOptions = {
+
+};
+refs.button.addEventListener('click', onClick)
+refs.form.addEventListener('input', onInput)
+
+function onInput(e) {
+  const value = e.target.value;
+  
+  
+  if (e.target.name === "delay") {
+    
+    promiseOptions.delay = Number(value);
+   
+    console.log( promiseOptions)
+  }
+    if (e.target.name === "step") {
+    
+    promiseOptions.step = Number(value);
+   
+    console.log( promiseOptions)
+    }
+   if (e.target.name === "amount") {
+    
+    promiseOptions.amount = Number(value);
+   
+    console.log( promiseOptions)
+   }
+  
+}
+
+function onClick(e) {
+  e.preventDefault();
+  console.log(promiseOptions)
+}
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   } else {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   }
+// }
+
